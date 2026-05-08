3810a95fb413ec574c53cd4b2399679d1495a193
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-08 22:01:13 +0200

    sign-up page edited. Form html and css done

--- DA-Bubble/src/app/features/auth/sign-up/sign-up.ts
+++ DA-Bubble/src/app/features/auth/sign-up/sign-up.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
 })
 export class SignUp {
-
+  goBack(){
+    window.history.back();
+  }
 }
