1c48c7c336bad1b96c9b681be0986871fcd21ef7
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-20 16:44:45 +0200

    login pages edited and form logic added

--- DA-Bubble/src/app/features/auth/login/login.ts
+++ DA-Bubble/src/app/features/auth/login/login.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+  onLogin() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.loginForm.reset();
+    console.log('Email:', email);
+    console .log('Password:', password);
+}
 }
