e668f2fd2eb3953ad4df387c5d444d76e910eeec
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-20 16:45:52 +0200

    forgot  paswort page edited

--- DA-Bubble/src/app/features/auth/forgot-password/forgot-password.ts
+++ DA-Bubble/src/app/features/auth/forgot-password/forgot-password.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
 export class ForgotPassword {
-
+    goBack(){
+    window.history.back();
+  }
 }
