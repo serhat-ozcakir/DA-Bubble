88cb781f3bebe4c9cbf1e889fa0c90d7046118e1
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-21 21:21:39 +0200

    auth service pages updated and methods written

--- DA-Bubble/src/app/core/services/auth.service.ts
+++ DA-Bubble/src/app/core/services/auth.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
 
+export interface RegisterData {
+
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
+
 export class Auth {
-  
+  private  RegisterData?: RegisterData;
+
+  setRegisterData(data: RegisterData) {
+    this.RegisterData = data;
+  }
+
+  getRegisterData(): RegisterData | undefined {
+    return this.RegisterData;
+  }
+
+  setAvatar(avatar: string) {
+    if (!this.RegisterData) return;
+    this.RegisterData.avatar = avatar;
+  }
+  clearRegisterData() {
+    this.RegisterData = undefined;
+  }
 }
