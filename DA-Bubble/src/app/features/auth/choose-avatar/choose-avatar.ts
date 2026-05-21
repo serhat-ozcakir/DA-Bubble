3892ab7b2113af6ec192ff6931e441713a14f1d3
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-21 21:20:40 +0200

    choose avatar pages edited and method, css styles written

--- DA-Bubble/src/app/features/auth/choose-avatar/choose-avatar.ts
+++ DA-Bubble/src/app/features/auth/choose-avatar/choose-avatar.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../../core/services/auth.service';
+import { Form, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-avatar',
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule],
   templateUrl: './choose-avatar.html',
   styleUrl: './choose-avatar.scss',
 })
 export class ChooseAvatar {
-  goBack(){
+
+  avatars = [
+    'assets/img/avatar/avatar-1.png',
+    'assets/img/avatar/avatar-2.png',
+    'assets/img/avatar/avatar-3.png',
+    'assets/img/avatar/avatar-4.png',
+    'assets/img/avatar/avatar-5.png',
+    'assets/img/avatar/avatar-6.png'
+  ];
+
+  selectedAvatar = 'assets/logo/Profile.png';
+  userName = '';
+
+ 
+  constructor(
+    private authService: Auth,
+    private router: Router
+  ) {
+
+    const registerData = this.authService.getRegisterData();
+
+    if (registerData) {
+      this.userName = registerData.name;
+    }
+  }
+
+  goBack() {
     window.history.back();
+  }
+
+  selectAvatar(avatar: string): void {
+    this.selectedAvatar = avatar; 
+  }
+
+  onChooseAvatar(){
+    this.authService.setAvatar(this.selectedAvatar);
+    console.log(this.authService.getRegisterData());
+    this.router.navigate(['/login']);
   }
 }
