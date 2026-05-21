0a557599d1ae27b70c87bbc5394ca9ab68bd2894
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-21 21:19:24 +0200

    signup form validation and css styles done

--- DA-Bubble/src/app/features/auth/sign-up/sign-up.ts
+++ DA-Bubble/src/app/features/auth/sign-up/sign-up.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router } from "@angular/router";
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Auth } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,10 +10,13 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   styleUrl: './sign-up.scss',
 })
 export class SignUp {
-  [x: string]: any;
+ // [x: string]: any;
   signUpForm: FormGroup;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private authService: Auth
+  ) {
     this.signUpForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/)]),
@@ -29,15 +33,16 @@ export class SignUp {
       this.signUpForm.markAllAsTouched();
       return;
     }
-    const name = this.signUpForm.get('name')?.value;
-    const email = this.signUpForm.get('email')?.value;
-    const password = this.signUpForm.get('password')?.value;
-    const terms = this.signUpForm.get('terms')?.value;
-    this.signUpForm.reset();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console .log('Password:', password);
-    console.log('Terms:', terms);
+   // const name = this.signUpForm.get('name')?.value;
+   // const email = this.signUpForm.get('email')?.value;
+   // const password = this.signUpForm.get('password')?.value;
+    //const terms = this.signUpForm.get('terms')?.value;
+   // this.signUpForm.reset();
+    this.authService.setRegisterData({
+      name: this.signUpForm.get('name')?.value,
+      email: this.signUpForm.get('email')?.value,
+      password: this.signUpForm.get('password')?.value,
+    });
     this.router.navigate(['/choose-avatar']);
   }
 
