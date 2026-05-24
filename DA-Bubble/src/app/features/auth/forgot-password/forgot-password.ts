72b77efe7f97deb1f643024c5db5cf0da370b2dd
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-24 15:41:29 +0200

    forgot-password pages edited und updated

--- DA-Bubble/src/app/features/auth/forgot-password/forgot-password.ts
+++ DA-Bubble/src/app/features/auth/forgot-password/forgot-password.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../../core/services/auth.service';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
 export class ForgotPassword {
-    goBack(){
+  forgotPasswordForm: FormGroup;
+
+  constructor(private router: Router, 
+    private authService: Auth) {
+    this.forgotPasswordForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  async onForgotPassword(): Promise<void> {
+    if(this.forgotPasswordForm.invalid) {
+      this.forgotPasswordForm.markAllAsTouched();
+      return;
+    }
+    const email = this.forgotPasswordForm.get('email')?.value;
+    try {
+      await this.authService.resetPassword(email);
+      console.log('Password reset email sent successfully');
+      this.router.navigate(['/send-email']);
+    } catch (error) {
+      console.error('Error sending password reset email:', error);
+    }
+  }
+
+  goBack(){
     window.history.back();
   }
 }
+
