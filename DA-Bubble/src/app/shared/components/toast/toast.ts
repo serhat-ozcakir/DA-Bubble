0d95cbd1d5dac661f1f65b808e1596983a9fb140
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-25 13:39:37 +0200

    toast component created and edited

---
+++ DA-Bubble/src/app/shared/components/toast/toast.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ToastService } from '../../../core/services/toast.service';
+
+@Component({
+  selector: 'app-toast',
+  imports: [],
+  templateUrl: './toast.html',
+  styleUrl: './toast.scss',
+})
+export class Toast {
+  constructor(public toastService: ToastService) {
+
+  }
+}
