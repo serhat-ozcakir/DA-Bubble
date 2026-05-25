2e186cde409b6ec2e4f2dc85edcac6369a40e1a9
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-25 19:34:37 +0200

    header pages edited

--- DA-Bubble/src/app/features/workspace/header/header.ts
+++ DA-Bubble/src/app/features/workspace/header/header.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../../core/services/auth.service';
+import {UserMenu} from './user-menu/user-menu';
+import {ProfileDialog} from './profile-dialog/profile-dialog';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [UserMenu, ProfileDialog],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
 
+  isUserMenuOpen = false;
+  isProfileMenuOpen = false;
+  isEditProfileDialogOpen = false;
+
+  constructor(public auth: Auth) {
+
+  }
+
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  openProfileMenu(): void {
+    this.isProfileMenuOpen = true;
+    this.isUserMenuOpen = false;
+  }
+
+  closeProfileMenu(): void {
+    this.isProfileMenuOpen = false;
+  }
 }
+
