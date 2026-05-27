9eaa95a0710e3ea90818e09743b3630fe8ac2559
serhat özcakir <serhatozcakir35@gmail.com>
2026-05-27 21:31:07 +0200

    edit profile dialog pages edited. onProfileName() function and events written

--- DA-Bubble/src/app/features/workspace/header/edit-profile-dialog/edit-profile-dialog.ts
+++ DA-Bubble/src/app/features/workspace/header/edit-profile-dialog/edit-profile-dialog.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Auth } from '../../../../core/services/auth.service';
+import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-edit-profile-dialog',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-profile-dialog.html',
   styleUrl: './edit-profile-dialog.scss',
 })
 export class EditProfileDialog {
+  @Output() closeDialogEvent = new EventEmitter<void>();
 
+  nameControl: FormControl<string>;
+
+  constructor(public auth: Auth) { 
+  this.nameControl = new FormControl(this.auth.currentUserProfile()?.name ?? '', 
+  {validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+
+  });
+
+  }
+
+  close(): void {
+    this.closeDialogEvent.emit();
+  }
+
+  async onProfileName(){
+    if(this.nameControl.invalid){
+      this.nameControl.markAsTouched();
+      return;
+    }
+    await this.auth.updateProfileName(this.nameControl.value);
+    this.close();
+  }
 }
