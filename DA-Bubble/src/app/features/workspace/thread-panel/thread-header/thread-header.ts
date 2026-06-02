0a823b52b301e7892f1cef34034e8a31803b62a2
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-02 20:36:45 +0200

    thread header component created

---
+++ DA-Bubble/src/app/features/workspace/thread-panel/thread-header/thread-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-header',
+  imports: [],
+  templateUrl: './thread-header.html',
+  styleUrl: './thread-header.scss',
+})
+export class ThreadHeader {
+
+}
