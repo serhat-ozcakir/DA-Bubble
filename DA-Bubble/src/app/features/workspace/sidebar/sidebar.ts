83fa7e315422493912cbd4705d79837e028e92dc
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-02 16:25:09 +0200

    sidebar pages edited and designed according to the concept

--- DA-Bubble/src/app/features/workspace/sidebar/sidebar.ts
+++ DA-Bubble/src/app/features/workspace/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Auth} from "../../../core/services/auth.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  isChannelsOpen = true;
+  isDirectMessagesOpen = true;
 
+ constructor(public auth: Auth) {
+ }
+
+ users = [
+  {
+    id: 1,
+    name: 'Frederik Beck (Du)',
+    avatar: 'assets/img/avatar/avatar-1.png',
+    status: 'online',
+  },
+  {
+    id: 2,
+    name: 'Sofia Müller',
+    avatar: 'assets/img/avatar/avatar-2.png',
+    status: 'online',
+  },
+  {
+    id: 3,
+    name: 'Noah Braun',
+    avatar: 'assets/img/avatar/avatar-3.png',
+    status: 'offline',
+  },
+];
+
+  toggleSection(section: 'channels' | 'directMessages'): void {
+    if (section === 'channels') {
+      this.isChannelsOpen = !this.isChannelsOpen;
+    } else if (section === 'directMessages') {
+      this.isDirectMessagesOpen = !this.isDirectMessagesOpen;
+    }
+  }
 }
