1b95ee1eb14b9d81775a65ae7ce14495a2dfd1b4
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-02 20:36:00 +0200

    thread-input component created

---
+++ DA-Bubble/src/app/features/workspace/thread-panel/thread-input/thread-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-input',
+  imports: [],
+  templateUrl: './thread-input.html',
+  styleUrl: './thread-input.scss',
+})
+export class ThreadInput {
+
+}
