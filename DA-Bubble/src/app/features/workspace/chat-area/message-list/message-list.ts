e05c3fc8a5d0fccfc2fc649085efaf896ae31c93
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-03 19:26:25 +0200

    message-loist pages edited

--- DA-Bubble/src/app/features/workspace/chat-area/message-list/message-list.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-list/message-list.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {  MessageItem, MessageItemComponent } from '../message-item/message-item';
 
 @Component({
   selector: 'app-message-list',
-  imports: [],
+  imports: [MessageItemComponent],
   templateUrl: './message-list.html',
   styleUrl: './message-list.scss',
 })
 export class MessageList {
-
+  messages: MessageItem[] = [
+    {
+    author: 'Noah Braun',
+    time: '14:25 Uhr',
+    text: 'Welche Version ist aktuell von Angular?',
+    avatar: 'assets/img/avatar/avatar-3.png',
+    isOwnMessage: false,
+    threadCount: 2
+    },
+      {
+      author: 'Frederik Beck',
+      time: '15:06 Uhr',
+      text: 'Ja das ist es.',
+      avatar: 'assets/img/avatar/avatar-1.png',
+      isOwnMessage: true,
+    },
+  ];
 }
