14052984632d9449433c3d82869ce55c0f7f1c7f
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-09 21:52:10 +0200

    thread-panel pages edited. messageService injected. Html and css pages updated

--- DA-Bubble/src/app/features/workspace/thread-panel/thread-panel.ts
+++ DA-Bubble/src/app/features/workspace/thread-panel/thread-panel.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MessageService} from '../../../core/services/message.service'
+import {ThreadHeader} from '../../../features/workspace/thread-panel/thread-header/thread-header'
+import {ThreadInput} from '../../../features/workspace/thread-panel/thread-input/thread-input'
+import {ThreadMessages} from '../../../features/workspace/thread-panel/thread-messages/thread-messages'
 
 @Component({
   selector: 'app-thread-panel',
-  imports: [],
+  imports: [ThreadHeader, ThreadInput,ThreadMessages],
   templateUrl: './thread-panel.html',
   styleUrl: './thread-panel.scss',
 })
 export class ThreadPanel {
+  messageService = inject(MessageService)
 
 }
