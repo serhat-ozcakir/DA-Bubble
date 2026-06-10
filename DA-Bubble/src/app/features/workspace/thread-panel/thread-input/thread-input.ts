a3e52a274a88e30b073d9b12fa3221162280a2de
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-10 13:41:07 +0200

    threaf input pages edited. sendReply() function added and html , css updated

--- DA-Bubble/src/app/features/workspace/thread-panel/thread-input/thread-input.ts
+++ DA-Bubble/src/app/features/workspace/thread-panel/thread-input/thread-input.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MessageService } from '../../../../core/services/message.service';
 
 @Component({
   selector: 'app-thread-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './thread-input.html',
   styleUrl: './thread-input.scss',
 })
 export class ThreadInput {
+  private messsageServis = inject(MessageService)
 
+  replyText = '';
+
+  async sendReply():Promise<void>{
+    const text = this.replyText.trim();
+    if(!text) return;
+    await this.messsageServis.sendThreadMessage(text);
+    this.replyText = '';
+  }
 }
