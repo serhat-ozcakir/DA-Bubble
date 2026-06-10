03b776588db97214c87ff48dc7094a02818b9b4d
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-10 14:19:04 +0200

    workspace layout edited. isThreadOpen() added and html , css pages updated

--- DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
+++ DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../header/header';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
 import { ChatArea } from '../chat-area/chat-area';
 import { ThreadPanel } from '../thread-panel/thread-panel';
 import { Auth } from '../../../core/services/auth.service';
+import { MessageService } from '../../../core/services/message.service';
 
 
 @Component({
   selector: 'app-workspace-layout',
-  imports: [Header, RouterOutlet, Sidebar, ChatArea,ThreadPanel],
+  imports: [Header, Sidebar, ChatArea,ThreadPanel],
   templateUrl: './workspace-layout.html',
   styleUrl: './workspace-layout.scss',
 })
 export class WorkspaceLayout {
 
   isSidebarClosed = false;
+  messageService = inject(MessageService)
 
   constructor(private auth: Auth) {
 
@@ -27,5 +29,9 @@ export class WorkspaceLayout {
 
   toggleSidebar(): void {
     this.isSidebarClosed = !this.isSidebarClosed;
+  }
+
+  get isThreadOpen():boolean{
+    return !!this.messageService.selectedThreadMessage()
   }
 }
