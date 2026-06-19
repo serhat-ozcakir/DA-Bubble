e255614af2a5314fd3b009698851dda5ccf92137
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-19 22:32:33 +0200

    emoji adde. Html and css pages edited. Ts page closeEmojiPickerOnOutsideClick(), closeEmojiPickerOnEscape() and addEmoji() functions written

--- DA-Bubble/src/app/features/workspace/chat-area/message-input/message-input.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-input/message-input.ts
@@ -1,42 +1,65 @@
-import { Component, inject } from '@angular/core';
+import { ElementRef, HostListener, Component, inject } from '@angular/core';
 import { MessageService } from '../../../../core/services/message.service';
 import { FormsModule } from '@angular/forms';
 import { DirectMessageService } from '../../../../core/services/direct-message.service';
 import { ChannelService } from '../../../../core/services/channel.service';
-
-
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 
 @Component({
   selector: 'app-message-input',
-  imports: [FormsModule],
+  imports: [FormsModule, PickerComponent],
   templateUrl: './message-input.html',
   styleUrl: './message-input.scss',
 })
 export class MessageInput {
-messageService = inject(MessageService);
-directMessageService = inject(DirectMessageService);
-channelService = inject(ChannelService)
-messageText: string = '';
+  private elementRef = inject(ElementRef);
+  messageService = inject(MessageService);
+  directMessageService = inject(DirectMessageService);
+  channelService = inject(ChannelService)
+  messageText: string = '';
+  showEmojiPicker = false;
+
+  async sendMessage(): Promise<void> {
+    console.log('SEND CLICKED, messageText:', this.messageText);
+    const text = this.messageText.trim();
+
+    if (!text) return;
 
 
+    if (this.directMessageService.currentDmUser()) {
+      console.log('DM gönderiliyor');
+      await this.directMessageService.sendDirectMessage(text);
+    } else {
+      console.log('Channel mesajı gönderiliyor');
+      await this.messageService.sendMessage(text);
+    }
 
-async sendMessage(): Promise<void> {
-  const text = this.messageText.trim();
-
-  if (!text) return;
-
-  console.log('DM USER:', this.directMessageService.currentDmUser());
-
-  if (this.directMessageService.currentDmUser()) {
-    console.log('DM gönderiliyor');
-    await this.directMessageService.sendDirectMessage(text);
-  } else {
-    console.log('Channel mesajı gönderiliyor');
-    await this.messageService.sendMessage(text);
+    this.messageText = '';
   }
 
-  this.messageText = '';
-}
+  @HostListener('document:click', ['$event'])
+  closeEmojiPickerOnOutsideClick(event: MouseEvent): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+    if (!clickedInside) {
+      this.showEmojiPicker = false;
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  closeEmojiPickerOnEscape(): void {
+    this.showEmojiPicker = false;
+  }
+
+  toggleEmojiPicker(): void {
+    this.showEmojiPicker = !this.showEmojiPicker
+  }
+
+  addEmoji(event: any): void {
+    const emoji = event.emoji.native || event.emoji.colons || '';
+    this.messageText += emoji;
+    this.showEmojiPicker = false;
+  }
 
 }
