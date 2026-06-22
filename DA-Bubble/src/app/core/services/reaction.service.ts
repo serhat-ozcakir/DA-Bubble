0571d07c9a8ea5ceb04d6954d13c24b348f491bc
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-22 21:20:00 +0200

    loadReactions() ,addReactions() ,getReactionForMessage() and removeReaction() functions writtten

--- DA-Bubble/src/app/core/services/reaction.service.ts
+++ DA-Bubble/src/app/core/services/reaction.service.ts
@@ -10,7 +10,7 @@ import { ReactionSummary } from '../models/reaction-summary.model'
 export class ReactionService {
   private supabase = inject(Supabase);
   private authService = inject(Auth);
-
+  readonly reactionOptions = ['✅', '👍', '🚀'];
   reactions = signal<MessageReaction[]>([]);
 
   reactionSummaries = computed<ReactionSummary[]>(() => {
@@ -38,4 +38,83 @@ export class ReactionService {
     }
     return Array.from(groupedReactions.values());
   })
+
+
+  async loadReactions(): Promise<void> {
+    const { data, error } = await this.supabase.supabase
+      .from('message_reactions')
+      .select('*')
+      .order('created_at', { ascending: true });
+
+    if (error) {
+      console.log('Error', error);
+      return;
+    }
+    this.reactions.set(data)
+  }
+
+  private async removeReaction(reactionId: string): Promise<void> {
+    const currentUser = this.authService.currentUserProfile();
+
+    console.log('Deleting reaction id:', reactionId);
+    console.log('Current user id:', currentUser?.id);
+
+    const { data, error } = await this.supabase.supabase
+      .from('message_reactions')
+      .delete()
+      .eq('id', reactionId)
+      .eq('user_id', currentUser?.id)
+      .select();
+
+    console.log('Delete result:', data);
+    console.log('Delete error:', error);
+
+    if (error) {
+      console.error('Fehler beim Entfernen der Reaction:', error);
+      return;
+    }
+
+    this.reactions.update((reactions) =>
+      reactions.filter((reaction) => reaction.id !== reactionId)
+    )
+  }
+
+  async addReaction(messageId: string, emoji: string): Promise<void> {
+    const currentUser = this.authService.currentUserProfile();
+
+    if (!currentUser) return;
+
+    const existingReaction = this.reactions().find(
+      (reaction) =>
+        reaction.message_id === messageId &&
+        reaction.user_id === currentUser.id &&
+        reaction.emoji === emoji
+    )
+
+    if (existingReaction) {
+      await this.removeReaction(existingReaction.id);
+      return;
+    }
+
+    const { error } = await this.supabase.supabase
+      .from('message_reactions')
+      .insert({
+        message_id: messageId,
+        user_id: currentUser.id,
+        emoji: emoji,
+      })
+    if (error) {
+      console.log('error', error);
+      return;
+    }
+    await this.loadReactions();
+  }
+
+
+  getReactionForMessage(messageId: string): ReactionSummary[] {
+    return this.reactionSummaries().filter(
+      (reaction) => reaction.messageId === messageId
+    )
+  }
+
 }
