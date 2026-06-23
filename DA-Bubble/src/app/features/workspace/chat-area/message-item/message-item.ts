aa2aab0797c882d2bc6f7ca7c6d9800a8c9ae710
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-23 19:57:53 +0200

    message item pages edited. According to figma design html and css updated. ReactionService injected

--- DA-Bubble/src/app/features/workspace/chat-area/message-item/message-item.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-item/message-item.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, Input } from '@angular/core';
 import { MessageView } from '../../../../core/models/message-view.model';
-import {MessageService} from '../../../../core/services/message.service'
+import {MessageService} from '../../../../core/services/message.service';
+import { ReactionService } from '../../../../core/services/reaction.service';
 
 @Component({
   selector: 'app-message-item',
@@ -11,7 +12,8 @@ import {MessageService} from '../../../../core/services/message.service'
 export class MessageItemComponent {
 
   message= input.required<MessageView>();
-  private messageService = inject(MessageService)
+  private messageService = inject(MessageService);
+  reactionService = inject(ReactionService);
 
   openThread():void{
     this.messageService.openThread(this.message())
