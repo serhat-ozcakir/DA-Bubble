07c9083dff2436080fc39a58cb6bda5bc43e5ad1
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-24 14:27:57 +0200

    message items pages edited.Emojis have been added to messages. The HTML and CSS have been adapted to the design. The functions closeEmojiPickerOnOutsideClick(), closeEmojiPickerOnEscape() and toggleEmojiPicker() have been added to the TS file.

--- DA-Bubble/src/app/features/workspace/chat-area/message-item/message-item.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-item/message-item.ts
@@ -1,11 +1,13 @@
-import { Component, inject, input, Input } from '@angular/core';
+import {ElementRef, HostListener, Component, inject, input, Input, signal } from '@angular/core';
 import { MessageView } from '../../../../core/models/message-view.model';
 import {MessageService} from '../../../../core/services/message.service';
 import { ReactionService } from '../../../../core/services/reaction.service';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+
 
 @Component({
   selector: 'app-message-item',
-  imports: [],
+  imports: [PickerComponent],
   templateUrl: './message-item.html',
   styleUrl: './message-item.scss',
 })
@@ -14,9 +16,35 @@ export class MessageItemComponent {
   message= input.required<MessageView>();
   private messageService = inject(MessageService);
   reactionService = inject(ReactionService);
+  isShowEmojiPicker = signal(false);
+  private elementRef = inject(ElementRef);
+
+  @HostListener('document:click', ['$event'])
+  closeEmojiPickerOnOutsideClick(event:Event){
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+    if(!clickedInside){
+      this.isShowEmojiPicker.set(false)
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  closeEmojiPickerOnEscape():void{
+    this.isShowEmojiPicker.set(false)
+  }
 
   openThread():void{
     this.messageService.openThread(this.message())
+  }
+
+  toggleEmojiPicker():void{
+    this.isShowEmojiPicker.update(value=>!value)
+  }
+  addEmojiReaction(event:any):void{
+    const emoji = event.emoji.native;
+
+    this.reactionService.addReaction(this.message().id, emoji);
+    this.isShowEmojiPicker.set(false);
   }
 
 }
