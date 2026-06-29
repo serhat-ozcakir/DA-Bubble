47681d2cd4402d8756e4fa8024a659188595a04b
serhat özcakir <serhatozcakir35@gmail.com>
2026-06-29 20:32:51 +0200

    thread panel pages edited. Messages can be updated.

--- DA-Bubble/src/app/features/workspace/thread-panel/thread-panel.ts
+++ DA-Bubble/src/app/features/workspace/thread-panel/thread-panel.ts
@@ -1,14 +1,15 @@
-import {ElementRef, HostListener, Component, inject, signal } from '@angular/core';
-import {MessageService} from '../../../core/services/message.service'
-import {ThreadHeader} from '../../../features/workspace/thread-panel/thread-header/thread-header'
-import {ThreadInput} from '../../../features/workspace/thread-panel/thread-input/thread-input'
-import {ThreadMessages} from '../../../features/workspace/thread-panel/thread-messages/thread-messages'
+import { ElementRef, HostListener, Component, inject, signal } from '@angular/core';
+import { MessageService } from '../../../core/services/message.service'
+import { ThreadHeader } from '../../../features/workspace/thread-panel/thread-header/thread-header'
+import { ThreadInput } from '../../../features/workspace/thread-panel/thread-input/thread-input'
+import { ThreadMessages } from '../../../features/workspace/thread-panel/thread-messages/thread-messages'
 import { ReactionService } from '../../../core/services/reaction.service';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { MessageView } from '../../../core/models/message-view.model';
 
 @Component({
   selector: 'app-thread-panel',
-  imports: [ThreadHeader, ThreadInput,ThreadMessages, PickerComponent],
+  imports: [ThreadHeader, ThreadInput, ThreadMessages, PickerComponent],
   templateUrl: './thread-panel.html',
   styleUrl: './thread-panel.scss',
 })
@@ -18,35 +19,71 @@ export class ThreadPanel {
   elementRef = inject(ElementRef);
   openedReactionPickerId = signal<string | null>(null);
   isMessageEdited = signal(false);
+  editingThreadMessageId = signal<string | null>(null);
+  editingThreadText = signal<string>('');
 
-  toggleThreadEmojiPicker(messageId:string, event:Event):void{
-    this.openedReactionPickerId.update((currentID)=>
-    currentID === messageId ? null : messageId)
+  toggleThreadEmojiPicker(messageId: string, event: Event): void {
+    this.openedReactionPickerId.update((currentID) =>
+      currentID === messageId ? null : messageId)
   }
 
-  addThreadEmojiReaction(event:any, messageId:string):void{
+  addThreadEmojiReaction(event: any, messageId: string): void {
     const emoji = event.emoji.native;
-    this.reactionService.addReaction(messageId,emoji);
+    this.reactionService.addReaction(messageId, emoji);
     this.openedReactionPickerId.set(null)
   }
 
   @HostListener('document:click', ['$event'])
-  closeEmojiPickerOnOutsideClick(event:Event):void{
+  closeEmojiPickerOnOutsideClick(event: Event): void {
     const clickedOutside = this.elementRef.nativeElement.contains(event?.target)
-    if(!clickedOutside){
+    if (!clickedOutside) {
       this.openedReactionPickerId.set(null);
       this.isMessageEdited.set(false);
+      this.cancelThreadEdit();
     }
   }
 
- @HostListener('document:keydown.escape')
- closeEmojiPickerOnEscape():void{
-  this.openedReactionPickerId.set(null);
-  this.isMessageEdited.set(false);
- }
+  @HostListener('document:keydown.escape')
+  closeEmojiPickerOnEscape(): void {
+    this.openedReactionPickerId.set(null);
+    this.isMessageEdited.set(false);
+    this.cancelThreadEdit();
+  }
 
- toggleMessageEdited():void{
-  this.isMessageEdited.update(value=>!value)
- }
+  toggleMessageEdited(): void {
+    this.isMessageEdited.update(value => !value)
+  }
+
+  startEditingThreadMessage(reply: MessageView, event: Event): void {
+    event.stopPropagation();
+
+    this.editingThreadMessageId.set(reply.id);
+    this.editingThreadText.set(reply.text);
+    this.isMessageEdited.set(false);
+  }
+
+  cancelThreadEdit(): void {
+    this.editingThreadMessageId.set(null);
+    this.editingThreadText.set('');
+  }
+
+  async saveThreadEditedMessage(): Promise<void> {
+
+    const messageId = this.editingThreadMessageId();
+
+    if (!messageId) return;
+    this.messageService.updateMessage(messageId, this.editingThreadText());
+
+    this.editingThreadMessageId.set(null);
+    this.editingThreadText.set('');
+  }
+
+  async saveOnEnter(event: KeyboardEvent): Promise<void> {
+    if (event.shiftKey) {
+      return;
+    }
+    event.preventDefault();
+    this.saveThreadEditedMessage();
+  }
 
 }
