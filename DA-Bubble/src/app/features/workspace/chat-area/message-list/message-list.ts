35c378da23857f8144feef3c098d131944b19e87
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-01 16:27:54 +0200

    subscribeToReactions() function added

--- DA-Bubble/src/app/features/workspace/chat-area/message-list/message-list.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-list/message-list.ts
@@ -1,11 +1,11 @@
 import { Component, effect, inject } from '@angular/core';
-import {  MessageItemComponent } from '../message-item/message-item';
+import { MessageItemComponent } from '../message-item/message-item';
 import { MessageView } from '../../../../core/models/message-view.model';
 import { MessageService } from '../../../../core/services/message.service';
 import { Auth } from '../../../../core/services/auth.service';
 import { ChannelService } from '../../../../core/services/channel.service';
 import { DirectMessageService } from '../../../../core/services/direct-message.service';
-import { ReactionService } from '../../../../core/services/reaction.service'; 
+import { ReactionService } from '../../../../core/services/reaction.service';
 
 @Component({
   selector: 'app-message-list',
@@ -14,35 +14,36 @@ import { ReactionService } from '../../../../core/services/reaction.service';
   styleUrl: './message-list.scss',
 })
 export class MessageList {
-  
+
   messageService = inject(MessageService);
   private authService = inject(Auth);
   private channelService = inject(ChannelService);
   directMessageService = inject(DirectMessageService);
   private reactionService = inject(ReactionService);
 
-constructor() {
-  effect(() => {
-    const dmUser = this.directMessageService.currentDmUser();
-    const channel = this.channelService.currentChannel();
-    this.reactionService.loadReactions();
+  constructor() {
+    this.reactionService.subscribeToReactions();
+    effect(() => {
+      const dmUser = this.directMessageService.currentDmUser();
+      const channel = this.channelService.currentChannel();
+      this.reactionService.loadReactions();
 
-    if (dmUser) {
-      this.directMessageService.loadDirectMessages();
-      this.directMessageService.listenToDirectMessages();
-      return;
-    }
+      if (dmUser) {
+        this.directMessageService.loadDirectMessages();
+        this.directMessageService.listenToDirectMessages();
+        return;
+      }
 
-    if (channel) {
-      this.directMessageService.removeDmRealtimeChannel();
-      this.messageService.loadMessages();
-      this.messageService.listenToMessages();
-    }
-  });
-}
+      if (channel) {
+        this.directMessageService.removeDmRealtimeChannel();
+        this.messageService.loadMessages();
+        this.messageService.listenToMessages();
+      }
+    });
+  }
 
   async ngOnInit(): Promise<void> {
-    await this.authService.loadCurrentUser(); 
+    await this.authService.loadCurrentUser();
   }
 
 }
