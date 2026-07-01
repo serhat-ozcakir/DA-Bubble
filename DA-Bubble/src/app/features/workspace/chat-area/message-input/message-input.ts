f9e625f39f80ecd3b8652fc191ccd0e6acc9089e
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-01 15:13:59 +0200

    message input pages edited. @ and # pop up added. According to design has been updated

--- DA-Bubble/src/app/features/workspace/chat-area/message-input/message-input.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/message-input/message-input.ts
@@ -1,9 +1,13 @@
-import { ElementRef, HostListener, Component, inject } from '@angular/core';
+import { ElementRef, HostListener, Component, inject, signal } from '@angular/core';
 import { MessageService } from '../../../../core/services/message.service';
 import { FormsModule } from '@angular/forms';
 import { DirectMessageService } from '../../../../core/services/direct-message.service';
 import { ChannelService } from '../../../../core/services/channel.service';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { UserService } from '../../../../core/services/user.service';
+import { Channel } from '../../../../core/models/channel.model';
+import { Profile } from '../../../../core/models/profile.model';
+
 
 
 @Component({
@@ -19,6 +23,10 @@ export class MessageInput {
   channelService = inject(ChannelService)
   messageText: string = '';
   showEmojiPicker = false;
+  userService = inject(UserService);
+  showMentionDropdown = signal(false);
+  mentionType = signal<'user' | 'channel' | null>(null);
+  mentionSearch = signal('');
 
   handleEnter(event: Event): void {
     event.preventDefault();
@@ -48,12 +56,14 @@ export class MessageInput {
     const clickedInside = this.elementRef.nativeElement.contains(event.target);
     if (!clickedInside) {
       this.showEmojiPicker = false;
+      this.showMentionDropdown.set(false);
     }
   }
 
   @HostListener('document:keydown.escape')
   closeEmojiPickerOnEscape(): void {
     this.showEmojiPicker = false;
+    this.showMentionDropdown.set(false);
   }
 
   toggleEmojiPicker(): void {
@@ -65,5 +75,82 @@ export class MessageInput {
     this.messageText += emoji;
     this.showEmojiPicker = false;
   }
+
+handleMentionInput(): void {
+  const text = this.messageText;
+
+  const lastAt = text.lastIndexOf('@');
+  const lastHash = text.lastIndexOf('#');
+
+  const isUserMention = lastAt > lastHash;
+  const mentionStartIndex = isUserMention ? lastAt : lastHash;
+
+  if (mentionStartIndex === -1) {
+    this.closeMentionDropdown();
+    return;
+  }
+
+  const trigger = text[mentionStartIndex];
+  const searchText = text.slice(mentionStartIndex + 1);
+
+  if (searchText.includes(' ')) {
+    this.closeMentionDropdown();
+    return;
+  }
+
+  this.mentionType.set(trigger === '@' ? 'user' : 'channel');
+  this.mentionSearch.set(searchText.toLowerCase());
+  this.showMentionDropdown.set(true);
+}
+
+closeMentionDropdown(): void {
+  this.showMentionDropdown.set(false);
+  this.mentionType.set(null);
+  this.mentionSearch.set('');
+}
+
+filteredUsers(): Profile[] {
+  const search = this.mentionSearch();
+
+  return this.userService.user().filter(user =>
+    user.name.toLowerCase().includes(search)
+  );
+}
+
+filteredChannels(): Channel[] {
+  const search = this.mentionSearch();
+
+  return this.channelService.channels().filter(channel =>
+    channel.name.toLowerCase().includes(search)
+  );
+}
+
+  insertMention(type:'@' | '#'):void{
+    this.messageText += type;
+
+    if(type === '@'){
+      this.mentionType.set('user');
+    } else{
+      this.mentionType.set('channel');
+    }
+    this.showMentionDropdown.set(true);
+  }
+
+selectUserMention(name: string): void {
+  const lastAt = this.messageText.lastIndexOf('@');
+  if (lastAt === -1) return;
+  this.messageText =
+    this.messageText.slice(0, lastAt) + `@${name} `;
+
+  this.closeMentionDropdown();
+}
+selectChannelMention(name: string): void {
+  const lastHash = this.messageText.lastIndexOf('#');
+  if (lastHash === -1) return;
+  this.messageText =
+    this.messageText.slice(0, lastHash) + `#${name} `;
+
+  this.closeMentionDropdown();
+}
 
 }
