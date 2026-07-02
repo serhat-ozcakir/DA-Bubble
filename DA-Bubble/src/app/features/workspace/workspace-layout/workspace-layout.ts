f3aaa8d196de4f2c0fad154e6e5ac031e7d3a046
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-02 21:57:48 +0200

    workspace-layout html and ts edited. According to desing HTML updated and TS showCreateChannelDialog signal and  openCreateChannelDialog() function written

--- DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
+++ DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../header/header';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
@@ -6,22 +6,23 @@ import { ChatArea } from '../chat-area/chat-area';
 import { ThreadPanel } from '../thread-panel/thread-panel';
 import { Auth } from '../../../core/services/auth.service';
 import { MessageService } from '../../../core/services/message.service';
+import {ChannelCreateDialog} from "../dialogs/channel-create-dialog/channel-create-dialog";
 
 
 @Component({
   selector: 'app-workspace-layout',
-  imports: [Header, Sidebar, ChatArea,ThreadPanel],
+  imports: [Header, Sidebar, ChatArea,ThreadPanel, ChannelCreateDialog],
   templateUrl: './workspace-layout.html',
   styleUrl: './workspace-layout.scss',
 })
 export class WorkspaceLayout {
 
   isSidebarClosed = false;
-  messageService = inject(MessageService)
+  messageService = inject(MessageService);
+  showCreateChannelDialog = signal(false);
 
-  constructor(private auth: Auth) {
 
-  }
+  constructor(private auth: Auth) {  }
 
   async ngOnInit() {
     await this.auth.loadCurrentUser();
@@ -34,4 +35,12 @@ export class WorkspaceLayout {
   get isThreadOpen():boolean{
     return !!this.messageService.selectedThreadMessage()
   }
+
+  openCreateChannelDialog():void{
+    this.showCreateChannelDialog.set(true)
+  }
+
+  closeCreateChannelDialog():void{
+    this.showCreateChannelDialog.set(false)
+  } 
 }
