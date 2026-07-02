a463fd430bc2711789030598e91ede9ccf11c50c
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-02 14:30:21 +0200

    channel-create-dialog components created

---
+++ DA-Bubble/src/app/features/workspace/dialogs/channel-create-dialog/channel-create-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-create-dialog',
+  imports: [],
+  templateUrl: './channel-create-dialog.html',
+  styleUrl: './channel-create-dialog.scss',
+})
+export class ChannelCreateDialog {
+
+}
