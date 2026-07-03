5052026e3efdb82be9362b5ea48b144275d0629a
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-03 13:18:20 +0200

    channel-create dialog pages edited. FormGroup and Validation integrated. HTML and SCSS updated

--- DA-Bubble/src/app/features/workspace/dialogs/channel-create-dialog/channel-create-dialog.ts
+++ DA-Bubble/src/app/features/workspace/dialogs/channel-create-dialog/channel-create-dialog.ts
@@ -1,22 +1,41 @@
-import { HostListener,Component, output, Host } from '@angular/core';
+import { HostListener, Component, output, Host } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-channel-create-dialog',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './channel-create-dialog.html',
   styleUrl: './channel-create-dialog.scss',
 })
 export class ChannelCreateDialog {
   closeDialog = output<void>();
+  channelForm = new FormGroup({
+    channelName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    channelDescription: new FormControl('', [Validators.maxLength(200)]),
+  });
 
-  constructor() {}
+  constructor() { }
 
   close(): void {
     this.closeDialog.emit();
-  } 
-@HostListener('document:keydown.escape') 
-  onEscape():void{
-    this.close();
-  } 
+  }
   
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    this.close();
+  }
+
+  createChannel(): void {
+    if (this.channelForm.invalid) {
+      this.channelForm.markAllAsTouched();
+      return;
+    }
+    const channelData = {
+      name: this.channelForm.get('channelName')?.value?.trim(),
+      description: this.channelForm.get('channelDescription')?.value?.trim() || '',
+    };
+
+    console.log('Channel Created:', channelData);
+    this.close();
+  }
 }
