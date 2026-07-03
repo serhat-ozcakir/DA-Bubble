654691939a569a95ae8cb6c15896518793bc4782
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-03 17:23:10 +0200

    add-members-dialog components created

---
+++ DA-Bubble/src/app/features/workspace/dialogs/add-members-dialag/add-members-dialag.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-members-dialag',
+  imports: [],
+  templateUrl: './add-members-dialag.html',
+  styleUrl: './add-members-dialag.scss',
+})
+export class AddMembersDialag {
+
+}
