a5d0814bd975d2d98c0b893bf17c96bf13228166
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-05 21:08:25 +0200

    add members dialog pages updated. According to design HTML, SCSS and TS  updated.

--- DA-Bubble/src/app/features/workspace/dialogs/add-members-dialag/add-members-dialag.ts
+++ DA-Bubble/src/app/features/workspace/dialogs/add-members-dialag/add-members-dialag.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, output, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ChannelService} from "../../../../core/services/channel.service";
+import {UserService} from "../../../../core/services/user.service";
+import { Profile } from '../../../../core/models/profile.model';
 
 @Component({
   selector: 'app-add-members-dialag',
@@ -7,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './add-members-dialag.scss',
 })
 export class AddMembersDialag {
+  closeAddMemberDialog = output<void>();
+  channelService = inject(ChannelService);
+  userService = inject(UserService);
+  searchText = signal('');
+  selectedUser = signal<Profile | null>(null);
+  users = this.userService.user;
 
+filteredUsers = computed(() => {
+    const search = this.searchText().toLowerCase().trim();
+  if(!search) {
+    return [];
+  }
+    return this.users().filter(user => user.name.toLowerCase().includes(search.toLowerCase()));
+  });
+
+  async ngOnInit(): Promise<void> {
+    await this.userService.loadUsers();
+  }
+
+  close(): void {
+    this.closeAddMemberDialog.emit();
+  }
+
+  selectUser(user: Profile): void {
+    this.selectedUser.set(user);
+    this.searchText.set(user.name);
+  }
+
+  clearSelectedUser():void {
+    this.selectedUser.set(null);
+    this.searchText.set('');
+  }
 }
