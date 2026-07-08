9c64ef1a7dc54d0572c623d273fd1976a98962ee
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-08 20:16:46 +0200

    add members list component created

---
+++ DA-Bubble/src/app/features/workspace/dialogs/add-members-list/add-members-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-members-list',
+  imports: [],
+  templateUrl: './add-members-list.html',
+  styleUrl: './add-members-list.scss',
+})
+export class AddMembersList {
+
+}
