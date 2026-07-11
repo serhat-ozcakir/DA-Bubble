ffc200bb8a818d7337ab19605bd3cf1213bc9e9a
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-11 17:34:01 +0200

    channel detail component created.

---
+++ DA-Bubble/src/app/features/workspace/dialogs/channel-detail/channel-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-detail',
+  imports: [],
+  templateUrl: './channel-detail.html',
+  styleUrl: './channel-detail.scss',
+})
+export class ChannelDetail {
+
+}
