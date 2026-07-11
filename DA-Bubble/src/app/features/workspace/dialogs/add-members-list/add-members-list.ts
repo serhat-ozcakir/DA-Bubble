49b9846c3e2558642da01776ffe2df33edb4b808
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-11 17:32:35 +0200

    add members list pages edited. toggleMembersDropdown() function written. According to design has been HTML and SCSS updated.

--- DA-Bubble/src/app/features/workspace/dialogs/add-members-list/add-members-list.ts
+++ DA-Bubble/src/app/features/workspace/dialogs/add-members-list/add-members-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, output, signal } from '@angular/core';
+import { ChannelService } from "../../../../core/services/channel.service";
+import { Auth } from "../../../../core/services/auth.service";
 
 @Component({
   selector: 'app-add-members-list',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './add-members-list.scss',
 })
 export class AddMembersList {
+  channelService = inject(ChannelService);
+  authService = inject(Auth);
+  isMembersDropdownOpen = signal(false);
+  closeAddMemberList = output<void>();
+  openAddMembers = output<void>();
+
+  toggleMembersDropdown(): void {
+    this.isMembersDropdownOpen.update((value) => !value);
+  }
 
 }
