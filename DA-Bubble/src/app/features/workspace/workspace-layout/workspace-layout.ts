32691088afb9876b65702654ed79b2eeb21f1fde
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-11 23:28:23 +0200

    workspace HTML and TS edited. openMemberListDialog(), closeAddMemberList(),openAddMembersFromList(), openChannelSettingsDialog() and   closeChannelSettingsDialog() functions has been written.

--- DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
+++ DA-Bubble/src/app/features/workspace/workspace-layout/workspace-layout.ts
@@ -6,14 +6,16 @@ import { ChatArea } from '../chat-area/chat-area';
 import { ThreadPanel } from '../thread-panel/thread-panel';
 import { Auth } from '../../../core/services/auth.service';
 import { MessageService } from '../../../core/services/message.service';
-import {ChannelCreateDialog} from "../dialogs/channel-create-dialog/channel-create-dialog";
-import {AddMembersDialag} from "../../workspace/dialogs/add-members-dialag/add-members-dialag";
+import { ChannelCreateDialog } from "../dialogs/channel-create-dialog/channel-create-dialog";
+import { AddMembersDialag } from "../../workspace/dialogs/add-members-dialag/add-members-dialag";
 import { ChannelService } from '../../../core/services/channel.service';
+import { AddMembersList } from '../dialogs/add-members-list/add-members-list';
+import { ChannelDetail } from "../dialogs/channel-detail/channel-detail";
 
 
 @Component({
   selector: 'app-workspace-layout',
-  imports: [Header, Sidebar, ChatArea,ThreadPanel, ChannelCreateDialog, AddMembersDialag],
+  imports: [Header, Sidebar, ChatArea, ThreadPanel, ChannelCreateDialog, AddMembersDialag, AddMembersList, ChannelDetail],
   templateUrl: './workspace-layout.html',
   styleUrl: './workspace-layout.scss',
 })
@@ -23,10 +25,12 @@ export class WorkspaceLayout {
   messageService = inject(MessageService);
   showCreateChannelDialog = signal(false);
   showAddMemberDialog = signal(false);
+  showMemberListDialog = signal(false);
+  showChannelSettingsDialog = signal(false);
   channelService = inject(ChannelService);
 
 
-  constructor(private auth: Auth) {  }
+  constructor(private auth: Auth) { }
 
   async ngOnInit() {
     await this.auth.loadCurrentUser();
@@ -38,23 +42,44 @@ export class WorkspaceLayout {
     this.isSidebarClosed = !this.isSidebarClosed;
   }
 
-  get isThreadOpen():boolean{
+  get isThreadOpen(): boolean {
     return !!this.messageService.selectedThreadMessage()
   }
 
-  openCreateChannelDialog():void{
+  openCreateChannelDialog(): void {
     this.showCreateChannelDialog.set(true)
   }
 
-  closeCreateChannelDialog():void{
+  closeCreateChannelDialog(): void {
     this.showCreateChannelDialog.set(false);
-  } 
+  }
 
-  openAddMembersDialog():void{
+  openAddMembersDialog(): void {
     this.showAddMemberDialog.set(true);
   }
-  closeAddMembersDialog():void{
+  closeAddMembersDialog(): void {
     console.log('calisti');
     this.showAddMemberDialog.set(false);
+  }
+
+  openMemberListDialog(): void {
+    this.showMemberListDialog.set(true);
+  }
+
+  closeAddMemberList(): void {
+    this.showMemberListDialog.set(false);
+  }
+
+  openAddMembersFromList(): void {
+    this.showAddMemberDialog.set(true);
+    this.showMemberListDialog.set(false);
+  }
+
+  openChannelSettingsDialog(): void {
+    this.showChannelSettingsDialog.set(true);
+  }
+
+  closeChannelSettingsDialog(): void {
+    this.showChannelSettingsDialog.set(false);
   }
 }
