9d1ab75c2839b7b8e96d6c4fc644fb18b898e05c
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-21 17:12:49 +0200

    header pages edited. The search input field in the header works based on the channel, user and messages entered

--- DA-Bubble/src/app/features/workspace/header/header.ts
+++ DA-Bubble/src/app/features/workspace/header/header.ts
@@ -7,6 +7,10 @@ import { Router } from '@angular/router';
 import { UserService } from '../../../core/services/user.service';
 import { Profile } from '../../../core/models/profile.model';
 import { DirectMessageService } from '../../../core/services/direct-message.service';
+import { ChannelService } from '../../../core/services/channel.service';
+import { Channel } from '../../../core/models/channel.model';
+import { Search } from '../../../core/services/search.service';
+import { SearchResult } from '../../../core/models/search-result.model';
 
 @Component({
   selector: 'app-header',
@@ -17,6 +21,8 @@ import { DirectMessageService } from '../../../core/services/direct-message.serv
 export class Header {
   private elementRef = inject(ElementRef);
   userService = inject(UserService);
+  channelService = inject(ChannelService);
+  searchService = inject(Search);
   directMessageService = inject(DirectMessageService);
   isUserMenuOpen = false;
   isProfileMenuOpen = false;
@@ -31,9 +37,26 @@ export class Header {
   async ngOnInit(): Promise<void> {
     await this.userService.loadUsers();
   }
+
   filteredUsers = computed(() => {
     const search = this.searchText().toLowerCase().trim();
     const users = this.userService.user();
+
+    if (search.startsWith('#')) {
+      return [];
+    }
+
+    if (search.startsWith('@')) {
+      const userSearch = search.slice(1).trim();
+
+      if (!userSearch) {
+        return users;
+      }
+
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(userSearch)
+      );
+    }
 
     if (!search) {
       return users;
@@ -43,6 +66,35 @@ export class Header {
       user.name.toLowerCase().includes(search)
     );
   });
+
+  filteredChannels = computed(() => {
+    const search = this.searchText().toLowerCase().trim();
+
+    if (!search.startsWith('#')) {
+      return [];
+    }
+
+    const channelSearch = search.slice(1).trim();
+    if (!channelSearch) {
+      return this.channelService.channels();
+    }
+    return this.channelService.channels().filter((channel) =>
+      channel.name.toLowerCase().includes(channelSearch)
+    )
+  })
+
+  searchMode = computed<'users' | 'channels' | 'messages'>(() => {
+    const search = this.searchText().trim();
+    if (!search || search.startsWith('@')) {
+      return 'users'
+    }
+
+    if (!search || search.startsWith('#')) {
+      return 'channels'
+    }
+
+    return 'messages'
+  })
 
   @HostListener('document:click', ['$event'])
   closeUserMenuOnOutsideClick(event: MouseEvent): void {
@@ -90,9 +142,17 @@ export class Header {
   }
 
 
-  onSearchInput(event: Event): void {
+  async onSearchInput(event: Event): Promise<void> {
     const input = event.target as HTMLInputElement;
-    this.searchText.set(input.value)
+    const value = input.value
+    this.searchText.set(value);
+    this.isSearchOpen.set(true);
+
+    if (this.searchMode() === 'messages') {
+      await this.searchService.searchChannelMessages(value)
+    } else {
+      this.searchService.clearSearch();
+    }
   }
 
   openSearch(): void {
@@ -108,5 +168,57 @@ export class Header {
     this.directMessageService.selectDmUSer(user);
     this.closeSearch();
   }
+
+  selectChannel(channel: Channel): void {
+    this.directMessageService.currentDmUser.set(null);
+    this.channelService.setCurrentChannel(channel);
+    this.closeSearch();
+  }
+
+  async selectMessageResult(result: SearchResult): Promise<void> {
+
+    if (result.type === 'channel-message') {
+      this.openChannelResult(result);
+    }
+
+    if (result.type === 'direct-message') {
+      this.openDirectMessageResult(result);
+    }
+  }
+
+  private openChannelResult(result: SearchResult): void {
+    if (!result.channelId) {
+      return;
+    }
+
+    const channel = this.channelService
+      .channels()
+      .find((channel) => channel.id === result.channelId);
+
+    if (!channel) {
+      return;
+    }
+
+    this.directMessageService.currentDmUser.set(null);
+    this.channelService.setCurrentChannel(channel);
+    this.closeSearch();
+  }
+
+  private async openDirectMessageResult(result: SearchResult): Promise<void> {
+    if (!result.profileId) {
+      return;
+    }
+
+    const user = this.userService.user()
+    .find((user)=> user.id === result.profileId)
+
+    if(!user){
+      return;
+    }
+    await this.directMessageService.selectDmUSer(user);
+    this.closeSearch();
+  }
+
+
 }
 
