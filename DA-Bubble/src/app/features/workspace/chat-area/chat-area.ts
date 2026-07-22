b0a239a8f9a82306da7c7dc1575a07d42ca54aa8
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-22 17:31:35 +0200

    chat area HTML and TS edited. showNewMessage signal added and Chat-area operates dynamically.

--- DA-Bubble/src/app/features/workspace/chat-area/chat-area.ts
+++ DA-Bubble/src/app/features/workspace/chat-area/chat-area.ts
@@ -1,11 +1,12 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import {ChatHeader} from "./chat-header/chat-header";
 import {MessageInput} from "./message-input/message-input";
 import {MessageList} from "./message-list/message-list";
+import { NewMessage } from './new-message/new-message';
 
 @Component({
   selector: 'app-chat-area',
-  imports: [ChatHeader, MessageInput, MessageList],
+  imports: [ChatHeader, MessageInput, MessageList,NewMessage],
   templateUrl: './chat-area.html',
   styleUrl: './chat-area.scss',
 })
@@ -14,6 +15,6 @@ export class ChatArea {
 openAddMembers = output<void>();
 openMemberList = output<void>();
 openChannelSettings = output<void>();
-
+showNewMessage = input(false);
 
 }
