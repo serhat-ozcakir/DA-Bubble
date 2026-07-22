c612b12ee59be051863e730f68d2821a9e4d831f
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-22 14:16:07 +0200

    new message component created

---
+++ DA-Bubble/src/app/features/workspace/chat-area/new-message/new-message.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-message',
+  imports: [],
+  templateUrl: './new-message.html',
+  styleUrl: './new-message.scss',
+})
+export class NewMessage {
+
+}
