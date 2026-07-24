3d0b2e9ac9ac4adeea3c7e3b2219d77ae45aabd7
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-24 16:05:11 +0200

    message-reaction models updated.

--- DA-Bubble/src/app/core/models/message-reaction.model.ts
+++ DA-Bubble/src/app/core/models/message-reaction.model.ts
@@ -1,6 +1,7 @@
-export interface MessageReaction{
+export interface MessageReaction {
   id: string;
-  message_id: string;
+  message_id: string | null;
+  direct_message_id: string | null;
   user_id: string;
   emoji: string;
   created_at: string;
