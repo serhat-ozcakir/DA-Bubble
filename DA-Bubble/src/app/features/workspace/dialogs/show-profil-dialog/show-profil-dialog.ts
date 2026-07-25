8d390f0f9f2b245c5ab4b7035ba64716b8952675
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-25 23:58:46 +0200

    show-profil components has been created

---
+++ DA-Bubble/src/app/features/workspace/dialogs/show-profil-dialog/show-profil-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-profil-dialog',
+  imports: [],
+  templateUrl: './show-profil-dialog.html',
+  styleUrl: './show-profil-dialog.scss',
+})
+export class ShowProfilDialog {
+
+}
