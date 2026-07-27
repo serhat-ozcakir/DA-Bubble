f3412d2b4425d1874506747a9555cc4e9ab7f923
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-27 14:55:07 +0200

    show-profil pages edited. According to Design has been updated HTML, TS and SCSS.

--- DA-Bubble/src/app/features/workspace/dialogs/show-profil-dialog/show-profil-dialog.ts
+++ DA-Bubble/src/app/features/workspace/dialogs/show-profil-dialog/show-profil-dialog.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HostListener, ElementRef, Component, inject, output } from '@angular/core';
+import { DirectMessageService } from '../../../../core/services/direct-message.service';
 
 @Component({
   selector: 'app-show-profil-dialog',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './show-profil-dialog.scss',
 })
 export class ShowProfilDialog {
+  directMessageService = inject(DirectMessageService);
+  elementRef = inject(ElementRef);
+  closeProfilDialogEvent = output<void>();
 
+  closeDialog(): void {
+    this.closeProfilDialogEvent.emit()
+  }
+
+  async openDirectMessage(): Promise<void> {
+    const user = this.directMessageService.currentDmUser();
+
+    if (!user) {
+      return;
+    }
+    await this.directMessageService.selectDmUser(user);
+    this.closeDialog();
+  }
 }
