526e0e248608b48680e20d3ca124b8319213ef21
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-28 23:29:24 +0200

    datenschutz and impressum components has been created.

---
+++ DA-Bubble/src/app/features/legal/datenschutz/datenschutz.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datenschutz',
+  imports: [],
+  templateUrl: './datenschutz.html',
+  styleUrl: './datenschutz.scss',
+})
+export class Datenschutz {
+
+}
---
+++ DA-Bubble/src/app/features/legal/impressum/impressum.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impressum',
+  imports: [],
+  templateUrl: './impressum.html',
+  styleUrl: './impressum.scss',
+})
+export class Impressum {
+
+}
