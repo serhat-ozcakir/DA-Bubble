2fce7f8ab8f79b80a7d556c61c954d6b55a85701
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-29 14:20:23 +0200

    auth footer HTML ,TS edited. Routerlink has been added and linked.

--- DA-Bubble/src/app/features/auth/auth-footer/auth-footer.ts
+++ DA-Bubble/src/app/features/auth/auth-footer/auth-footer.ts
@@ -1,10 +1,11 @@
 import { CommonModule, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-auth-footer',
-  imports: [NgStyle, CommonModule],
+  imports: [NgStyle, CommonModule, RouterLink],
   templateUrl: './auth-footer.html',
   styleUrl: './auth-footer.scss',
 })
