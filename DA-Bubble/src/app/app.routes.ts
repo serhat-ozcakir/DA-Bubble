0652b0d29989dbaecdba73585f59b45ac5a817bb
serhat özcakir <serhatozcakir35@gmail.com>
2026-07-29 13:31:45 +0200

    app.route.ts updated. New paths have been created for the Impressum ve Datenschutz

--- DA-Bubble/src/app/app.routes.ts
+++ DA-Bubble/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
+import { Impressum } from './features/legal/impressum/impressum';
+import { Datenschutz } from './features/legal/datenschutz/datenschutz';
 
 export const routes: Routes = [
+    {
+        path:'impressum',
+        component: Impressum
+    },
+    {
+        path:'datenschutz',
+        component: Datenschutz
+    },
+
     {
         path:'',
         loadChildren:()=> import('./features/auth/auth.routes').then(m=>m.Auth_Routes)
